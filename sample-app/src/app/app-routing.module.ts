ba81c54ff6f03afd038cbc36019ce19780c75d32
sean1093 <sean1093@gmail.com>
2017-12-18 10:55:12 +0800

    update

--- sample-app/src/app/app-routing.module.ts
+++ sample-app/src/app/app-routing.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {ContentComponent} from './content/content.component';
-import {PictureComponent} from './picture/picture.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/content', pathMatch: 'full' },
-  { path: 'picture', component: PictureComponent },
   { path: 'content', component: ContentComponent }
 ];
 
